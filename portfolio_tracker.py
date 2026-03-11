86d58d61d82b738e8b7f79e0a9a1b07443884842
thomchapuis <77327313+thomchapuis@users.noreply.github.com>
2026-03-11 16:11:35 +0100

    Update portfolio_tracker.py

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -1,4 +1,7 @@
 import yfinance as yf
+from datetime import datetime
+
+now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # ETF CAC40
 ticker5 = yf.Ticker("CACC.PA")
@@ -16,6 +19,7 @@ gain_eur = evolution*quantity5
 gain_pct = round((evolution/buy_price)*100,2)
 total_PTF = quantity5*infos.get('regularMarketPrice')
 
+print(f"Run time: {now}")
 print(f"gain Amundi ETF CAC40: {gain_eur}€, soit {gain_pct}%")
 print(f"portefeuille PEA: {total_PTF}")
 print("------")
